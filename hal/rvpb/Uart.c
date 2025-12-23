1d5527b93ba98beefbdd6711ad009c48c9344b13
jiwon <idjjm95@gmail.com>
2025-12-23 11:39:28 +0900

    by interrupt

---
+++ hal/rvpb/Uart.c
@@ -0,0 +1,48 @@
+#include "stdint.h"
+#include "Uart.h"
+#include "HalUart.h"
+#include "HalInterrupt.h"
+
+extern volatile PL011_t* Uart;
+
+static void interrupt_handler();
+
+void Hal_uart_init(){
+    Uart->uartcr.bits.UARTEN = 0;
+    Uart->uartcr.bits.TXE=1;
+    Uart->uartcr.bits.RXE=1;
+    Uart->uartcr.bits.UARTEN=1;
+
+    // Enable input interrupt
+    Uart->uartimsc.bits.RXIM = 1; // 이걸 했어야 되는구나.
+
+    Hal_interrupt_enable(UART_INTERRUPT0);
+    Hal_interrupt_register_handler(interrupt_handler, UART_INTERRUPT0);
+}
+
+void Hal_uart_put_char(uint8_t ch)
+{
+    while(Uart->uartfr.bits.TXFF);
+    Uart->uartdr.all = (ch & 0xFF);
+}
+
+uint8_t Hal_uart_get_char(){
+    uint32_t data;
+
+    while(Uart->uartfr.bits.RXFE);
+
+    data=Uart->uartdr.all;
+
+    if(data & 0xFFFFFF00){
+        // clear the error
+        Uart->uartrsr.all = 0xFF;
+        return 0;
+    }
+
+    return (uint8_t)(data & 0xFF);
+}
+
+static void interrupt_handler(){
+    uint8_t ch = Hal_uart_get_char();
+    Hal_uart_put_char(ch);
+}
---
+++ lib/armcpu.h
@@ -0,0 +1,9 @@
+#ifndef LIB_ARM_CPU_H_
+#define LIB_ARM_CPU_H_
+
+void enable_irq();
+void enable_fiq();
+void disable_irq();
+void disable_fiq();
+
+#endif
